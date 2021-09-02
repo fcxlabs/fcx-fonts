a69a4cee7a4f05d6b688b9608c49815219bbdc46
fernando.lopes <fernando.lopes@ferreiracosta.com.br>
2021-09-02 18:52:44 -0300

    removed lib directory and added it rto .gitigonre

--- lib/index.d.ts
+++
@@ -1,5 +0,0 @@
-export { red } from "./colors/main/red";
-export { green } from "./colors/main/green";
-export { pear } from "./colors/main/pear";
-export { gray } from "./colors/main/gray";
-//# sourceMappingURL=index.d.ts.map
