1672f9f6a44c5659bcaa8102bcfc363b492fb9d9
fernando.lopes <fernando.lopes@ferreiracosta.com.br>
2021-09-03 11:49:15 -0300

    adicinano a interface color para index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export { red } from "./colors/main/red";
 export { green } from "./colors/main/green";
 export { pear } from "./colors/main/pear";
 export { gray } from "./colors/main/gray";
+export { Color } from "./types/colors";
